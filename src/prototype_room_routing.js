a975df7938fcfaccbc366917b4265aa2d1014464
Tobias Wilken <tooangel@tooangel.de>
2017-01-21 21:06:29 +0100

    Set upgrader position before others

--- src/prototype_room_routing.js
+++ src/prototype_room_routing.js
@@ -85,6 +85,13 @@ Room.prototype.updatePosition = function() {
     extractor: []
   };
 
+  if (this.controller) {
+    let upgraderPos = this.controller.pos.findNearPosition().next().value;
+    this.memory.position.creep[this.controller.id] = upgraderPos;
+    costMatrixBase.set(upgraderPos.x, upgraderPos.y, config.layout.creepAvoid);
+    this.setMemoryCostMatrix(costMatrixBase);
+  }
+
   let sources = this.find(FIND_SOURCES);
   for (let source of sources) {
     let sourcer = source.pos.findNearPosition().next().value;
@@ -108,11 +115,6 @@ Room.prototype.updatePosition = function() {
       this.memory.position.structure.extractor.push(mineral.pos);
     }
 
-    let upgraderPos = this.controller.pos.findNearPosition().next().value;
-    this.memory.position.creep[this.controller.id] = upgraderPos;
-    costMatrixBase.set(upgraderPos.x, upgraderPos.y, config.layout.creepAvoid);
-    this.setMemoryCostMatrix(costMatrixBase);
-
     let storagePos = this.memory.position.creep[this.controller.id].findNearPosition().next().value;
     this.memory.position.structure.storage.push(storagePos);
     // TODO should also be done for the other structures
@@ -127,7 +129,7 @@ Room.prototype.updatePosition = function() {
     let pathUpgrader = this.getPath(route, 0, 'pathStart', this.controller.id, true);
     // TODO exclude the last position (creepAvoid) in all paths
     for (let pos of pathUpgrader) {
-      if (upgraderPos.isEqualTo(pos.x, pos.y)) {
+      if (this.memory.position.creep[this.controller.id].isEqualTo(pos.x, pos.y)) {
         continue;
       }
       costMatrixBase.set(pos.x, pos.y, config.layout.pathAvoid);
